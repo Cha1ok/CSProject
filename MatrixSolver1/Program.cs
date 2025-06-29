6bd3fad444300ee5e35854d46ea82a75a28a2bac
Cha1ok <kuptsov2004@list.ru>
2025-06-29 13:31:47 +0300

    Matrix

---
+++ MatrixSolver1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MatrixSolver1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Solver solver = new Solver(n);
+            solver.SetMatrix();
+            int[][] Tm =
+            {
+                new int[] {1,2,3},
+                new int[] { 4, 5, 6 },
+                new int[] { 6, 7, 8 }
+            };
+
+            Tm = solver.SumMatrix(Tm);
+            solver.PrintMaxtrix(Tm);
+        }
+    }
+}
---
+++ MatrixSolver1/Solver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MatrixSolver1
+{
+    internal class Solver
+    {
+        private int _n;
+        private int[][] _a;  
+
+        public Solver(int n)
+        {
+            _n = n;
+            _a = new int[n][];  
+            for (int i = 0; i < n; i++)
+            {
+                _a[i] = new int[n];  
+            }
+        }
+        
+        public void SetMatrix()
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    _a[i][j] = Convert.ToInt32(Console.Read());
+                }
+            }
+        }
+
+        public void PrintMaxtrix(int[][] x)
+        {
+            for(int i = 0;i < x.Length; i++)
+            {
+                Console.WriteLine("\n");
+                for(int j = 0; j < x[i].Length;j++)
+                {
+                    Console.Write(x[i][j]+" ");
+                }
+            }
+        }
+
+
+        public int[][] SumMatrix(int[][] x)
+        {
+            int[][]t=new int[_n][];
+            for(int i = 0;i<x.Length;i++)
+            {
+                t[i]=new int[_n];
+                for(int j = 0; j < x[i].Length; j++)
+                {
+                    t[i][j] = x[i][j] + _a[i][j];
+                }
+            }
+            return t;
+        }
+
+        public int[][] ProizvMatrix(int[][] x)
+        {
+            int [][]t=new int[_n][];
+            for(int i = 0; i < x.Length; i++)
+            {
+                t[i]=new int[x[0].Length];
+                for(int j = 0;j < x[i].Length; j++)
+                {
+                    t[i][j] = 0;
+                    for(int k = 0; k < _a[0].Length; k++)
+                    {
+                        t[i][j] += x[i][k] * _a[k][j];
+                    }
+                }
+            }
+            return t;
+        }
+    }
+}
