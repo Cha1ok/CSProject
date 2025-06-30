98604de2a28aa2153ccaeb17bd1e8387701340e0
Cha1ok <kuptsov2004@list.ru>
2025-06-30 15:19:59 +0300

    ED

---
+++ EnDecrypt/ED.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnDecrypt
+{
+    public static class ED
+    {
+        private const int Shift = 3;
+
+        public static string Encrypt(string message)
+        {
+            char[] chars = message.ToCharArray();
+            for(int i=0;i<chars.Length;i++)
+            {
+                char c = chars[i];
+                if (char.IsLetter(c))
+                {
+                    char basechar = char.IsUpper(c) ? 'A' : 'a';
+                    chars[i]=(char)(((c-basechar+Shift)%26)+basechar);
+                }
+            }
+            return new string(chars);
+        }
+
+        public static string Decrypt(string message)
+        {
+            char[] chars = message.ToCharArray();
+            for(int i=0;i< chars.Length;i++)
+            {
+                char c = chars[i];
+                if (char.IsLetter(c))
+                {
+                    char basechar=char.IsUpper(c) ? 'A' : 'a';
+                    chars[i]=(char)(((c-basechar-Shift+26)%26)+basechar) ;
+                }
+            }
+            return new string((chars));
+        }
+    }
+}
---
+++ EnDecrypt/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EnDecrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            string en=ED.Encrypt(message);
+            Console.WriteLine("Zashifr "+ en);
+            string ed=ED.Decrypt(message);
+            Console.WriteLine("Rashfr "+ ed);
+        }
+    }
+}
